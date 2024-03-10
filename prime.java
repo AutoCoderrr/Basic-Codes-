88466c21b5019d8218fb6f1a6c1a27f960e2394f
Anshul <68611365+AutoCoderrr@users.noreply.github.com>
2024-03-10 20:24:26 +0530

    Add files via upload

---
+++ prime.java
@@ -0,0 +1,35 @@
+// number divisible by one and itself 
+
+import java.util.Scanner;
+
+/**
+ * prime
+ */
+public class prime {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a number ");
+        int num = sc.nextInt();
+        
+        int count =0;
+
+        for(int i=1;i<=num;i++)
+
+        {
+            if(num%i==0)
+            {
+                count++;
+            }
+            
+
+        }
+        if(count==2)
+        System.out.println(num +" is a prime number ");
+        else {
+            System.out.println(num + " is not a prime number ");
+        }
+
+    }
+}
